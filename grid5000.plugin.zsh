a630a89b6b9b7689ed878283de70da905d0f0c34
Sebastien Badia <seb@sebian.fr>
2012-12-07 00:52:52 +0100

    Oar, add time remaning

--- grid5000.plugin.zsh
+++ grid5000.plugin.zsh
@@ -12,3 +12,14 @@ oar_job_id () {
     echo "($OAR_JOB_ID)"
   fi
 }
+
+# Compute and display Job remaning time
+oar_remaining_time(){
+  if [ -n "$OAR_JOB_WALLTIME_SECONDS" -a -n "$OAR_NODE_FILE" -a -r "$OAR_NODE_FILE" ]; then
+    DATE_NOW=$(date +%s)
+    DATE_JOB_START=$(stat -c %Y $OAR_NODE_FILE)
+    DATE_TMP=$OAR_JOB_WALLTIME_SECONDS
+    ((DATE_TMP = (DATE_TMP - DATE_NOW + DATE_JOB_START) / 60))
+    echo -n "$DATE_TMP"
+  fi
+}
